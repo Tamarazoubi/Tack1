8838e1e434529d74942e7bc7ae70d40ae539d43b
Tamarazoubi <tamarazoubi2021@gmail.com>
2025-03-29 22:20:24 +0300

    add

--- Task/lib/stack.dart
+++ Task/lib/stack.dart
@@ -11,28 +11,52 @@ class _stackClassState extends State<stackClass> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: GridView.count(crossAxisCount: 2,
-
-      children: [
-        Stack(
+      appBar: AppBar(
+        backgroundColor: Colors.purple[900],
+        title: Column(
+        children: [        Row(
+           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
+            Text("Aduit"),
+            Text("sign in"),
+          Icon(IconData(0xe3de, fontFamily: 'MaterialIcons'))
+
 
           ],
-        )
+
+        ),
+
+          Row(
+            children: [
+             Stack(
+              children: [
+                Container(
+
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    image: DecorationImage(image:
+NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd2ZZn1O7P6wpLivC46RDkkpL4uN862KR9Lw&s')
+
+                    ),
+
+),
+
+),
+
+
+              ],
+            ),
+
         ],
+          )
+            ],
 
-
-      )
-      
       ),
+      )
+
+
+
     );
   }
-}
-Widget box(){
-  return Container(
-    height: 150,
-    width: 150
-    ,color: Colors.pink,
-  );
 }
 
